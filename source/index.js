17b9c46112f99dfeb16352d16fc4c9037bc20828
darthmorgot <dartmorgot@yandex.ru>
2023-07-03 19:33:58 +1000

    Добавил кода в файл для теста страницы проекта

--- source/index.js
+++ source/index.js
@@ -1,1 +1,18 @@
+import MainComponent from './js/MainComponent.js';
+
+const component = MainComponent('My component text');
+
+document.querySelector('body').appendChild(component);
+
+const init = () => {
+  const div = document.getElementById('content');
+  if(div){
+    div.textContent = 'Text is added by javascript and webpack!!!';
+  }
+};
+
+init();
+
+console.log(`2 * 4 = ${2 * 4}`);
+
 console.log('Test index page.');
