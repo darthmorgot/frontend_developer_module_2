72af909be4dcb38cfd6570da7931a906b4cded6a
darthmorgot <dartmorgot@yandex.ru>
2023-07-04 23:49:02 +1000

    Добавил в конфиг пути для других страниц проекта

--- webpack.config/webpack.multipage.js
+++ webpack.config/webpack.multipage.js
@@ -2,6 +2,8 @@ result = {};
 
 result.entry = {
   pageIndex: './source/pages/index.pug',
+  pageCatalog: './source/pages/catalog.pug',
+  pageContest: './source/pages/contest.pug',
 };
 
 module.exports = result;
