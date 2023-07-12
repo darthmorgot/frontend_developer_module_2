93b669c5f3021fd416ad84fee8c9a826aeb77035
darthmorgot <dartmorgot@yandex.ru>
2023-07-12 18:48:12 +1000

    Мелкие правки скриптов.

--- source/index.js
+++ source/index.js
@@ -2,17 +2,6 @@ import MainComponent from './js/MainComponent.js';
 
 const component = MainComponent('My component text');
 
-document.querySelector('body').appendChild(component);
-
-const init = () => {
-  const div = document.getElementById('content');
-  if(div){
-    div.textContent = 'Text is added by javascript and webpack!!!';
-  }
-};
-
-init();
-
 console.log(`2 * 4 = ${2 * 4}`);
-
+console.log(component);
 console.log('Test index page.');
--- source/js/MainComponent.js
+++ source/js/MainComponent.js
@@ -1,11 +1,5 @@
 const MainComponent = (text = 'Default text') => {
-    const box = document.createElement('div');
-    box.classList.add('main-component-box');
-    box.style.border = "1px solid #CCC";
-
-    box.textContent = text;
-
-    return box;
+  return text;
 }
 
 export default MainComponent;
