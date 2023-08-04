6eaca5cc062fde6126bf81bd6e015e94a6504051
darthmorgot <dartmorgot@yandex.ru>
2023-08-05 00:04:00 +1000

    Мелкие правки кода.

--- source/index.js
+++ source/index.js
@@ -1,7 +1,1 @@
-import MainComponent from './js/MainComponent.js';
-
-const component = MainComponent('My component text');
-
-console.log(`2 * 4 = ${2 * 4}`);
-console.log(component);
 console.log('Test index page.');
