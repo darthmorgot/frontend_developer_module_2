cbde5fa83b3ca7254ccd102308e1860087cbdd0f
darthmorgot <dartmorgot@yandex.ru>
2023-09-10 00:47:58 +1000

    Добавил настройки для обработки изображений.

--- webpack.config/webpack.prod.js
+++ webpack.config/webpack.prod.js
@@ -1,11 +1,46 @@
 const TerserPlugin = require('terser-webpack-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+const ImageWebpPlugin = require('imagemin-webp-webpack-plugin');
 
 module.exports = {
   mode: 'production',
   devtool: false,
   optimization: {
-    minimize: true,
-    minimizer: [new TerserPlugin()],
+    minimizer: [
+      new TerserPlugin(),
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            plugins: [
+              'imagemin-gifsicle',
+              'imagemin-mozjpeg',
+              'imagemin-pngquant',
+              ['imagemin-svgo', {
+                plugins: [
+                  {
+                    name: 'preset-default',
+                    params: {
+                      overrides: {
+                        removeViewBox: false,
+                        addAttributesToSVGElement: {
+                          params: {
+                            attributes: [
+                              {xmlns: 'http://www.w3.org/2000/svg'},
+                            ],
+                          },
+                        },
+                      },
+                    },
+                  },
+                ],
+              }],
+            ],
+          },
+        },
+      }),
+      new ImageWebpPlugin(),
+    ],
   },
   performance: {
     hints: false,
