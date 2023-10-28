8f0cfa93a71b18a9943e932a821eb4ec76742ef0
darthmorgot <dartmorgot@yandex.ru>
2023-10-28 19:32:46 +1000

    Создал новый файл для задач gulp.

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+const {src, dest, parallel} = require('gulp');
+const svgSprite = require('gulp-svg-sprite');
+const webp = require('gulp-webp');
+const changed = require('gulp-changed');
+
+function sprite() {
+  return src([
+    './source/img/icons/*.svg',
+    './source/img/logo/*.svg',
+    '!./source/img/icons/icons-sprite.svg',
+    '!./source/img/logo/logo-sprite.svg'
+  ])
+    .pipe(changed('./source/img/'))
+    .pipe(svgSprite({
+      mode: {
+        stack: {
+          dest: '',
+          sprite: 'sprite.svg'
+        },
+      }
+    }))
+    .pipe(dest('./source/img/'));
+}
+
+function toWebp() {
+  return src(['./source/img/**/*.{jpg,png}', '!./source/img/favicons/*.png'])
+    .pipe(changed('./source/img/'))
+    .pipe(webp({quality: 75}))
+    .pipe(dest('./source/img/'));
+}
+
+exports.sprite = sprite;
+exports.toWebp = toWebp;
+exports.getSpriteWebp = parallel(sprite, toWebp);
