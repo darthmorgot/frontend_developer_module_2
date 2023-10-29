98f4da0f91512bc3da37cc0c8e6cfcfb1311e7e7
darthmorgot <dartmorgot@yandex.ru>
2023-10-29 21:39:15 +1000

    Исправил код в связи с удалением ненужных плагинов.

--- webpack.config/webpack.common.js
+++ webpack.config/webpack.common.js
@@ -2,7 +2,6 @@ const path = require('./paths.js');
 const multipage = require('./webpack.multipage.js');
 
 const pugPlugin = require('./plugins/pug-plugin-module.js');
-const fileManagerPlugin = require('./plugins/file-manager-plugin.js');
 
 const htmlPreset = require('./presets/html-preset-common.js');
 const stylePreset = require('./presets/style-preset-common.js');
@@ -10,8 +9,6 @@ const stylePresetProd = require('./presets/style-preset-prod.js');
 const scriptPreset = require('./presets/script-preset-common.js');
 const imagePreset = require('./presets/image-preset-common.js');
 const fontPreset = require('./presets/font-preset-common.js');
-
-const ImageWebpPlugin = require('imagemin-webp-webpack-plugin');
 
 const devMode = process.env.BUILD_TYPE !== 'production';
 
@@ -33,8 +30,6 @@ module.exports = {
   },
   plugins: [
     pugPlugin(devMode),
-    fileManagerPlugin(devMode),
-    devMode ? new ImageWebpPlugin() : null,
   ],
   module: {
     rules: [
