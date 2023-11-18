ad481dda1f3b26267c49147b259839f48d4f40b5
darthmorgot <dartmorgot@yandex.ru>
2023-11-19 00:54:10 +1000

    Удалил вызов функции, вместо него добавил экспорт этой же функции, и мелкие правки кода.

--- source/js/open-close-menu.js
+++ source/js/open-close-menu.js
@@ -1,45 +1,41 @@
-(function () {
-  'use strict';
+/**
+ * Управление (открытие-закрытие) меню кнопкой-гамбургером
+ */
+function openCloseMenu() {
+  let header = document.querySelector('.header');
+  let nav = document.querySelector('.nav');
+  let btn = document.querySelector('.nav__menu-button');
+  let navList = document.querySelector('.nav__list');
+  let navTop = document.querySelector('.nav__top');
 
-  /**
-   * Управление (открытие-закрытие) меню кнопкой-гамбургером
-   */
-  function openCloseMenu() {
-    let header = document.querySelector('.header');
-    let nav = document.querySelector('.nav');
-    let btn = document.querySelector('.nav__menu-button');
-    let navList = document.querySelector('.nav__list');
-    let navTop = document.querySelector('.nav__top');
+  header.classList.remove('header--nojs');
+  nav.classList.remove('nav--nojs');
 
-    header.classList.remove('header--nojs');
-    nav.classList.remove('nav--nojs');
-
-    btn.addEventListener('click', () => {
-      for (let i = 0; i < btn.children.length - 1; i++) {
-        if (btn.children[i].classList.contains('toggle')) {
-          btn.children[i].classList.remove('toggle');
-          addRemoveClass(btn, 'nav__menu-button--open', 'nav__menu-button--close');
-        } else {
-          btn.children[i].classList.add('toggle');
-          addRemoveClass(btn, 'nav__menu-button--close', 'nav__menu-button--open');
-        }
+  btn.addEventListener('click', () => {
+    for (let i = 0; i < btn.children.length - 1; i++) {
+      if (btn.children[i].classList.contains('toggle')) {
+        btn.children[i].classList.remove('toggle');
+        addRemoveClass(btn, 'nav__menu-button--open', 'nav__menu-button--close');
+      } else {
+        btn.children[i].classList.add('toggle');
+        addRemoveClass(btn, 'nav__menu-button--close', 'nav__menu-button--open');
       }
+    }
 
-      navList.classList.toggle('toggle');
-      navTop.classList.toggle('nav__top--close-menu');
-    });
-  }
+    navList.classList.toggle('toggle');
+    navTop.classList.toggle('nav__top--close-menu');
+  });
+}
 
-  /** Добавление-удаление класса элементу, в данном случае кнопке-гамбургеру
-   * для отображения открытия или закрытия меню
-   * @param elem кнопка-гамбургер
-   * @param removeClass удаляемый класс
-   * @param addClass добавляемый класс
-   */
-  function addRemoveClass(elem, removeClass, addClass) {
-    elem.classList.remove(removeClass);
-    elem.classList.add(addClass);
-  }
+/** Добавление-удаление класса элементу, в данном случае кнопке-гамбургеру
+ * для отображения открытия или закрытия меню
+ * @param elem кнопка-гамбургер
+ * @param removeClass удаляемый класс
+ * @param addClass добавляемый класс
+ */
+function addRemoveClass(elem, removeClass, addClass) {
+  elem.classList.remove(removeClass);
+  elem.classList.add(addClass);
+}
 
-  openCloseMenu();
-})();
+module.exports = openCloseMenu;
